9c4f7011d741440bc7c40c49776512585d7cdc73
Andy Dill <andy.dill@gmail.com>
2020-06-11 12:38:51 -0700

    filler in another context

--- preload.js
+++ preload.js
@@ -10,3 +10,50 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+
+function maker(w, h) {
+  return new window.ImageData(w, h);
+}
+
+const vm = require('vm');
+
+const context = vm.createContext(Object.create(null));
+const _requireModule = vm.runInContext(
+  `
+  var width = 1280;
+  var height = 720;
+
+  var id1 = null;
+  var id2 = null;
+
+  var oneOrTwo = 1;
+
+  function requireModule(maker) {
+    if (id1 == null) {
+      id1 = maker(width, height);
+    }
+    if (id2 == null) {
+      id2 = maker(width, height);
+    }
+    var idata = (oneOrTwo === 1) ? id1 : id2;
+    oneOrTwo = (oneOrTwo === 1) ? 2 : 1;
+
+    var data = idata.data;
+    var bytes = width * height * 4;
+    for (var i = 0; i < bytes; i += 4) {
+      data[i+0] = Math.random() * 255;
+      data[i+1] = Math.random() * 255;
+      data[i+2] = Math.random() * 255;
+      data[i+3] = 255;
+    }
+
+    return idata;
+  }
+  requireModule
+`,
+  context
+);
+
+window.getAnotherFrame = function () {
+  return _requireModule(maker);
+}
--- renderer.js
+++ renderer.js
@@ -8,28 +8,8 @@
 var myCanvas = document.getElementById("pad");
 var myContext = myCanvas.getContext("2d");
 
-var width = 1280;
-var height = 720;
-
-var id1 = new window.ImageData(width, height);
-var id2 = new window.ImageData(width, height);
-
-var oneOrTwo = 1;
-
 function draw(timestamp) {
-  var idata = (oneOrTwo === 1) ? id1 : id2;
-  oneOrTwo = (oneOrTwo === 1) ? 2 : 1;
-
-  var data = idata.data;
-  var bytes = width * height * 4;
-  for (var i = 0; i < bytes; i += 4) {
-    data[i+0] = Math.random() * 255;
-    data[i+1] = Math.random() * 255;
-    data[i+2] = Math.random() * 255;
-    data[i+3] = 255;
-  }
-
-  myContext.putImageData(idata, 0, 0);
+  myContext.putImageData(window.getAnotherFrame(), 0, 0);
 
   window.requestAnimationFrame(draw);
 }
